8a7e8dc29f90857d663f3f451f0b99b9c0848233
sharmapreetish <128233814+sharmapreetish@users.noreply.github.com>
2023-06-07 09:41:20 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteOutlinedIcon from "@material-ui/icons/EventNoteOutlined";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <EventNoteOutlinedIcon />
+        Note-Maker
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
